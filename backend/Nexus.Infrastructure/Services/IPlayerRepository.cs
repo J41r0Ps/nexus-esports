e665e76b098ebb527e370efc82d36ed4bc1c80ac
Jairo Nacurena <r1067776@student.thomasmore.be>
2026-06-26 19:55:04 +0200

    player repository

---
+++ backend/Nexus.Infrastructure/Services/IPlayerRepository.cs
@@ -0,0 +1,29 @@
+﻿using Nexus.Domain.Entities;
+
+namespace Nexus.Infrastructure.Services
+{
+    public interface IPlayerRepository
+    {
+        Task<IEnumerable<Player>> GetPlayersAsync(
+            string? searchQuery,
+            string? role,
+            int? teamId,
+            int? countryId,
+            int pageNumber,
+            int pageSize);
+
+        Task<int> GetPlayerCountAsync(
+            string? searchQuery,
+            string? role,
+            int? teamId,
+            int? countryId);
+
+        Task<Player?> GetPlayerAsync(int playerId);
+        Task<bool> PlayerExistsAsync(int playerId);
+        Task<bool> TeamExistsAsync(int teamId);
+        Task<bool> CountryExistsAsync(int countryId);
+        void AddPlayer(Player player);
+        void DeletePlayer(Player player);
+        Task SaveChangesAsync();
+    }
+}
